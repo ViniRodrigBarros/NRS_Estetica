122377cf21dfb190bb76c7fe96b5332ca694cffe
vinicius rodrigues barros <95061193+ViniRodrigBarros@users.noreply.github.com>
2022-03-08 22:02:40 -0300

    delete

---
+++ banco de dados/delete.sql
@@ -0,0 +1,20 @@
+delete from  empresas where id_empresa>'1';
+select * from empresas;
+
+delete from  pessoas where id_pessoa>'1';
+select * from pessoas;
+
+delete from  clientes where id_cliente>'1';
+select * from clientes;
+
+delete from  socios where id_socio>'1';
+select * from socios;
+
+delete from agendamentos where id_agendamentos>'1';
+select * from agendamentos;
+
+delete from servicos where id_servicos>'1';
+select * from servicos;
+
+delete from servicos_agedamentos where id_serviços_agedamentos>'1';
+select * from servicos_agedamentos;
--- insertsql.sql
+++ insertsql.sql
@@ -3,7 +3,7 @@ insert into pessoas (nome, telefone, email, nascimento, fk_empresa) values('?', 
 insert into clientes (obs, fk_pessoa) values("?",1);
 insert into socios (obs, senha, usuario, fk_pessoas) values('?','?','?',1);
 insert into agendamentos ( obs, realizado, pago, preço_total, desconto, fk_cliente, fk_socio) values ('?','0',0,0,0,1,1);
-insert into agendamentos (data_hora) values ( STR_TO_DATE('01/01/2000 00:00:00','%d/%m/%Y h:%i:%s')); 
+insert into agendamentos (data_hora) values ( STR_TO_DATE('01/01/2000 00:00:00','%d/%m/%Y h:%i:%s'));
 insert into servicos(nome ,tempo ,obs , preço ,desconto, fk_socio1) values ('?',"00:00:00",'?',1,1,1);
 insert into servicos_agedamentos(fk_agendamentos,fk_servicos) values(1,1);
 ''
